c3eca62c1b4d61204426fe7e52dc93e97473544d
Daniel M. Sahu <danielmohansahu@gmail.com>
2022-05-14 07:20:20 -0400

    Initial commit of basic plugin infrastructure; cleaned up and reformatted from examples.

---
+++ include/FindROSPrimitivesPlugin.h
@@ -0,0 +1,93 @@
+/* CLANG Compiler Plugin for ROS Primitive Extraction.
+ *
+ * Class declarations.
+ *
+ * Author: Daniel M. Sahu
+ */
+
+#pragma once
+
+// STL
+
+// CLANG
+#include "clang/Frontend/FrontendPluginRegistry.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendAction.h"
+#include "clang/Tooling/Tooling.h"
+
+namespace find_ros_primitives
+{
+
+/* Custom Recursive AST Visitor to analyze each declaration in the AS Tree.
+ *
+ * This class does the core evaluation of whether or not a given declaration
+ * is a ROS primitive.
+ */
+class FindROSPrimitivesVisitor : public clang::RecursiveASTVisitor<FindROSPrimitivesVisitor>
+{
+ public:
+  explicit FindROSPrimitivesVisitor(clang::ASTContext *Context)
+    : Context(Context) {}
+
+  /* Evaluate a single declaration.
+   */
+  bool VisitCXXRecordDecl(clang::CXXRecordDecl *Declaration);
+ 
+ private:
+  clang::ASTContext *Context;
+};
+
+/* Custom AST Consumer to configure calls to the Visitor.
+ */
+class FindROSPrimitivesConsumer : public clang::ASTConsumer
+{
+ public:
+  FindROSPrimitivesConsumer(clang::ASTContext *Context,
+                            clang::CompilerInstance &Instance,
+                            std::set<std::string> ParsedTemplates)
+      : Visitor(Context), Instance(Instance), ParsedTemplates(ParsedTemplates)
+  {}
+
+  /* Core method to analyze a given element in the AST.
+   */
+  virtual void HandleTranslationUnit(clang::ASTContext &Context)
+  {
+    Visitor.TraverseDecl(Context.getTranslationUnitDecl());
+  }
+
+ private:
+  // Some of these are currently unused, but may be required at later stages of development.
+  clang::CompilerInstance &Instance;
+  std::set<std::string> ParsedTemplates;
+  FindROSPrimitivesVisitor Visitor;
+};
+
+/* AST Plugin Action - this is our top level class that interfaces with the Compiler.
+ */
+class FindROSPrimitivesAction : public clang::PluginASTAction
+{
+ protected:
+
+  /* Factory method to return our custom consumer.
+   */
+  std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(clang::CompilerInstance &CI,
+                                                        llvm::StringRef) override
+  {
+    return std::make_unique<FindROSPrimitivesConsumer>(&CI.getASTContext(), CI, ParsedTemplates);
+  }
+
+  /* Parse command line input arguments.
+   */
+  bool ParseArgs(const clang::CompilerInstance &CI, const std::vector<std::string> &args) override;
+
+  /* Human readable help usage.
+   */
+  void PrintHelp(llvm::raw_ostream& oss);
+
+ private:
+  std::set<std::string> ParsedTemplates;
+};
+
+} // namespace find_ros_primitives
---
+++ src/FindROSPrimitivesPlugin.cpp
@@ -0,0 +1,79 @@
+/* CLANG Compiler Plugin for ROS Primitive Extraction.
+ *
+ */
+
+// CLANG
+#include "clang/Frontend/FrontendPluginRegistry.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendAction.h"
+#include "clang/Tooling/Tooling.h"
+
+// CUSTOM
+#include "FindROSPrimitivesPlugin.h"
+
+using namespace clang;
+
+namespace find_ros_primitives
+{
+
+bool FindROSPrimitivesVisitor::VisitCXXRecordDecl(clang::CXXRecordDecl *Declaration)
+{
+  if (Declaration->getQualifiedNameAsString() == "ros::init")
+  {
+    FullSourceLoc FullLocation = Context->getFullLoc(Declaration->getBeginLoc());
+    if (FullLocation.isValid())
+      llvm::outs() << "Found declaration at "
+                   << FullLocation.getSpellingLineNumber() << ":"
+                   << FullLocation.getSpellingColumnNumber() << "\n";
+  }
+  return true;
+}
+
+bool FindROSPrimitivesAction::ParseArgs(const clang::CompilerInstance &CI,
+                                        const std::vector<std::string> &args)
+{
+  // handle input command line arguments one at a time
+  for (unsigned i = 0, e = args.size(); i != e; ++i)
+  {
+    llvm::errs() << "FindROSPrimitives arg = " << args[i] << "\n";
+
+    // Example error handling.
+    clang::DiagnosticsEngine &D = CI.getDiagnostics();
+    if (args[i] == "-an-error")
+    {
+      unsigned DiagID = D.getCustomDiagID(DiagnosticsEngine::Error,
+                                          "invalid argument '%0'");
+      D.Report(DiagID) << args[i];
+      return false;
+    }
+    else if (args[i] == "-parse-template")
+    {
+      if (i + 1 >= e)
+      {
+        D.Report(D.getCustomDiagID(DiagnosticsEngine::Error,
+                                   "missing -parse-template argument"));
+        return false;
+      }
+      ++i;
+      ParsedTemplates.insert(args[i]);
+    }
+  }
+  if (!args.empty() && args[0] == "help")
+    PrintHelp(llvm::errs());
+
+  return true;
+}
+
+void FindROSPrimitivesAction::PrintHelp(llvm::raw_ostream& oss)
+{
+  oss << "Help for FindROSPrimitives plugin goes here\n";
+}
+
+} // namespace find_ros_primitives
+
+// plugin registration
+static FrontendPluginRegistry::Add<find_ros_primitives::FindROSPrimitivesAction>
+X("find-ros-primitives", "find location of ROS primitives");
+
